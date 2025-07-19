58fe5da4513410080cc8422342f6acb5bcf26f96
Gibmyx <gibmyxjobs@gmail.com>
2025-07-19 11:07:16 -0400

    first

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return redirect('dashboard');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
